b2c015f86a5fcfcedfd52d5bd7b0a6315f1bb6f9
Rachel <rachelgroff176@gmail.com>
2019-05-18 16:32:00 -0500

    updated dependency and styles

--- src/app/todo.component.ts
+++ src/app/todo.component.ts
@@ -3,15 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['../assets/css/bootstrap.min.css']
+  styleUrls: ['../assets/css/bootstrap.min.css', './todo.component.css']
 })
 export class TodoComponent implements OnInit {
   title: string; 
+  
+  task: string;
+  tasks = [];
 
+  onClick() {
+    this.tasks.push({ name: this.task });
+    this.task = '';
+  }
+
+  onEdit(item) {
+    this.task = item;
+  }
+  
   constructor() { }
 
   ngOnInit() {
     this.title = "To Do List";
   }
+
 
 }
